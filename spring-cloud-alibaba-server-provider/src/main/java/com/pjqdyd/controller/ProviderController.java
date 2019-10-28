8dd085609002a644dc4668717e0598cb990a7254
pjqdyd <1909025079@qq.com>
2019-10-28 20:00:53 +0800

    测试提供者负载均衡

--- spring-cloud-alibaba-server-provider/src/main/java/com/pjqdyd/controller/ProviderController.java
+++ spring-cloud-alibaba-server-provider/src/main/java/com/pjqdyd/controller/ProviderController.java
@@ -1,5 +1,6 @@
 package com.pjqdyd.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,9 +14,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class ProviderController {
 
+    @Value("${server.port}")
+    private String port;
+
     @GetMapping("/sayHi/{name}")
     public String sayHi(@PathVariable("name") String name){
-        return "Hello from " + name;
+        return "Hello from " + port + name;
     }
 
 }
