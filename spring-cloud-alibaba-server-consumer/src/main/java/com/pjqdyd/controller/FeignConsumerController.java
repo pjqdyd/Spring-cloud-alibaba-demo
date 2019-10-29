1be91836d436bbf1ee6bef31bfae559a4f861078
pjqdyd <1909025079@qq.com>
2019-10-29 17:56:35 +0800

    change

--- spring-cloud-alibaba-server-consumer/src/main/java/com/pjqdyd/controller/FeignConsumerController.java
+++ spring-cloud-alibaba-server-consumer/src/main/java/com/pjqdyd/controller/FeignConsumerController.java
@@ -2,8 +2,6 @@ package com.pjqdyd.controller;
 
 import com.pjqdyd.service.MyFeignService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,24 +12,15 @@ import org.springframework.web.bind.annotation.RestController;
  * @Version:      [v1.0.0]
  */
 
-@RefreshScope
 @RestController
 public class FeignConsumerController {
 
     @Autowired
     private MyFeignService myFeignService;
 
-    @Value("${user.admin}")
-    private String admin;
-
     @GetMapping("/feign/sayHi/{name}")
     public String sayHi(@PathVariable("name") String name){
         return myFeignService.sayHi(name);
-    }
-
-    @GetMapping("/feign/getAdmin")
-    public String getAdmin(){
-        return admin;
     }
 
 }
