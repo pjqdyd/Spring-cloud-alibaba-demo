d650d50a8d2b5a332e76919878a5264899fcf08d
pjqdyd <1909025079@qq.com>
2019-11-01 17:16:18 +0800

    dubbo配置负载均衡

--- spring-cloud-alibaba-dubbo-provider/spring-cloud-alibaba-dubbo-provider-service/src/main/java/com/pjqdyd/service/SayHelloServiceImpl.java
+++ spring-cloud-alibaba-dubbo-provider/spring-cloud-alibaba-dubbo-provider-service/src/main/java/com/pjqdyd/service/SayHelloServiceImpl.java
@@ -2,6 +2,7 @@ package com.pjqdyd.service;
 
 import com.pjqdyd.api.SayHelloService;
 import org.apache.dubbo.config.annotation.Service;
+import org.springframework.beans.factory.annotation.Value;
 
 /**   
  * @Description:  [dubbo接口服务实现类]
@@ -11,8 +12,12 @@ import org.apache.dubbo.config.annotation.Service;
 @Service(version = "1.0.0")
 public class SayHelloServiceImpl implements SayHelloService {
 
+
+    @Value("${dubbo.protocol.port}")
+    public String port;
+
     @Override
     public String sayHi(String name) {
-        return "Hello From Dubbo " + name;
+        return "Hello From Dubbo " + port  + name;
     }
 }
